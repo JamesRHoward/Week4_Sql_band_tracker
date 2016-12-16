b22d99136a289241ec5b5ca5aa5e6dd4ac9fcfca
James Howard <james@>
2016-12-16 10:47:25 -0800

    Add Constructor, Getters and setters, and GetAll Mehtod and test to Band.cs

---
+++ Objects/Band.cs
@@ -0,0 +1,57 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace BandTracker
+{
+  public class Band
+  {
+    private int _id;
+    private string _name;
+
+    public Band(string Name, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+    }
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string Name)
+    {
+      _name = Name;
+    }
+    public static List<Band> GetAll()
+    {
+      List<Band> allBands = new List<Band>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM bands;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int bandId = rdr.GetInt32(0);
+        string bandName = rdr.GetString(1);
+        Band newBand = new Band(bandName, bandId);
+        allBands.Add(newBand);
+      }
+      if(rdr!=null)
+      {
+        rdr.Close();
+      }
+      if(conn!=null)
+      {
+        conn.Close();
+      }
+      return allBands;
+    }
+  }
+}
--- Tests/BandTest.cs
+++ Tests/BandTest.cs
@@ -1,7 +1,24 @@
-// using Xunit;
-// using System.Collections.Generic;
-// using System;
-// using System.Data;
-// using System.Data.SqlClient;
-//
-// namespace BandTracker
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BandTracker
+{
+  public class BandTest
+  {
+    public BandTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=DESKTOP-86RQO71;Initial Catalog=band_tracker_test;Integrated Security=SSPI;";
+    }
+    [Fact]
+    public void Band_TestBandIsEmptyAtFirst()
+    {
+      //Arrange, Act
+      int result = Band.GetAll().Count;
+
+      Assert.Equal(0, result);
+    }
+  }
+}
