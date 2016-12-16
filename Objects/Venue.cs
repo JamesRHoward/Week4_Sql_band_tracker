cb812a7de692579d6d071d8325acc0b6106362bb
James Howard <james@>
2016-12-16 08:40:51 -0800

    Add getters and setters and GetAll method to Venue.cs and test for GetAll method

---
+++ Objects/Venue.cs
@@ -0,0 +1,58 @@
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System;
+
+namespace BandTracker
+{
+  public class Venue
+  {
+    private int _id
+    private string _name;
+
+    public Venue(string Name, int Id = 0)
+    {
+      _id = Id;
+      _name = Name;
+    }
+
+    public int GetId()
+    {
+      return _id;
+    }
+    public string GetName()
+    {
+      return _name;
+    }
+    public void SetName(string Name)
+    {
+      _name = Name;
+    }
+
+    public static List<Venue> GetAll()
+    {
+      List<Venue> allVenues = new List<Venue>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM venues;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int venueId = rdr.GetInt32(0);
+        string venueName = rdr.GetString(1);
+        Venue newVenue = new Venue(venueName, venueId);
+        allVenues.Add(newVenue);
+      }
+      if(rdr!=null)
+      {
+        rdr.Close();
+      }
+      if(conn!=null)
+      {
+        conn.Close();
+      }
+    }
+  }
+}
---
+++ Tests/VenueTest.cs
@@ -0,0 +1,23 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace BandTracker
+{
+  public class VenueTest
+  {
+    public VenueTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=DESKTOP-86RQO71;Initial Catalog=template_test;Integrated Security=SSPI;";
+    }
+    [Fact]
+    public void Venue_TestVenueIsEmptyAtFirst()
+    {
+      int result = Venue.GetAll().Count;
+
+      Assert.Equal(0, result);
+    }
+  }
+}
